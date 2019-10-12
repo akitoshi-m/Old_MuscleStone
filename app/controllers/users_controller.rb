6021ef388e02d0dee1ebda642bfe90ce8390e266
EC2 Default User <ec2-user@ip-172-31-18-221.us-east-2.compute.internal>
2019-10-12 04:31:11 +0000

    ユーザーのupdateアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,19 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'ユーザー編集が完了しました'
+      redirect_to edit_user_path(@user)
+    else
+      render 'edit'
+    end
   end
   
   private
