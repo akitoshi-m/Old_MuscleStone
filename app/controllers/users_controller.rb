edaf60d036cdd61dbe793d90ebc16958fe9296bd
EC2 Default User <ec2-user@ip-172-31-18-221.us-east-2.compute.internal>
2019-10-12 04:12:57 +0000

    ユーザー詳細ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def show
-
+    @user = User.find(params[:id])
   end
-
     
   def create
     @user = User.new(user_params)
@@ -16,6 +15,10 @@ class UsersController < ApplicationController
       flash.now[:danger] = "登録に失敗しました"
       render :new
     end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
   end
   
   private
